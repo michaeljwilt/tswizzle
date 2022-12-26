f2edce1873253c30f1b46a220a119fe0385a9ec0
Michael Wilt <michaeljwilt@outlook.com>
2022-12-26 11:25:33 -0600

    Add files via upload
    Initial Wrangling Code

---
+++ wrangle.py
@@ -0,0 +1,87 @@
+
+#--------------------Importing--------------------#
+#Import Packages
+#%%
+import pandas as pd
+
+
+#Import Dataset
+#%%
+swift = pd.read_csv("taylor_swift_spotify.csv")
+swift.head()
+
+
+
+
+#--------------------Data Wrangling--------------------#
+
+#Drop Uneccessary Columns
+#%%
+swift.drop(["id","uri"], axis=1, inplace=True)
+
+
+#Convert release_date from object to datetime
+#%%
+swift["release_date"] = pd.to_datetime(swift["release_date"])
+swift.release_date.info
+
+
+
+#Convert our song duration from milliseconds to minutes in a new column
+#%%
+swift["duration_min"] =  round((swift["duration_ms"] / 1000) / 60, 2)
+
+
+# #Commented out duration_ms drop so we can use and compare it's significance later
+# #%%
+# swift.drop("duration_ms", axis=1, inplace=True)
+
+
+
+
+
+#--------------------Data Exploration--------------------#
+
+#View Initial Stats for all variables
+#%%
+swift.describe()
+
+
+##Things to notice and explore further
+    # 1. Instrumentalness min and 25% is 0.000
+    # 2. Popularity has a min of 0
+
+#%%
+swift.loc[swift["instrumentalness"] == 0]
+#While instrumentalness has 0 score for these, popularity varies as do other variables
+#I think we should keep these in since it may play a role
+
+
+#Let's explore popularity
+swift.loc[swift["popularity"] == 0]
+#It appears that a majority of these songs are either karaoke or voice memo versions
+#We should remove these since it will affect our statistical analysis and ML models
+
+
+
+
+#Create new variable dropping songs with popularity of 0
+#%%
+swift1 = swift.loc[swift["popularity"] != 0]
+swift1.shape
+
+#Comparing the shape to ensure we made a change
+#%%
+swift.shape
+
+
+#Verifying our songs with 0 popularity were removed
+#%%
+swift1.loc[swift["popularity"] == 0]
+
+
+
+
+#Correlation Matrix to see correlation between our variables
+#%%
+swift1.corr()
